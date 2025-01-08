d30d2edb6e69c02a40c2ca7ea71a3f8a80099854
Daniel F <danielfranco2011@gmail.com>
2025-01-07 20:44:28 -0800

    Create vs Cmake project

---
+++ Pokemon_Battle_CLI.cpp
@@ -0,0 +1,12 @@
+﻿// Pokemon_Battle_CLI.cpp : Defines the entry point for the application.
+//
+
+#include "Pokemon_Battle_CLI.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
