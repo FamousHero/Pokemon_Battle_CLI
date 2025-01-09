747ca51a7905705160fb3bffcfd0042d69164e81
Daniel F <danielfranco2011@gmail.com>
2025-01-09 02:16:52 -0800

    Add csv Data files, copy to build dir, display pokemon.csv

--- Pokemon_Battle_CLI.cpp
+++ Pokemon_Battle_CLI.cpp
@@ -1,12 +1,55 @@
 ﻿// Pokemon_Battle_CLI.cpp : Defines the entry point for the application.
 //
 
-#include "Pokemon_Battle_CLI.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
 
-using namespace std;
+// Could store in a db but thats too complicated for rn
+// Creating a hash set to live in RAM since its only 35kb
+// Means file must be included with binary
+
+// May make hash map with values being possible moves to 
+
+std::unordered_set<std::string> CreatePokedex() {
+	std::ifstream pokemon("pokemon.csv");
+	std::ifstream pokemon_stats("pokemon_stats.csv");
+	
+	std::string line;
+	while(pokemon.good()) {
+		std::getline(pokemon, line);
+		std::cout << line << "\n";
+	}
+	std::cout << std::flush;
+	std::unordered_set<std::string> test_pokedex{"test", "pokedex"};
+	pokemon.close();
+	return test_pokedex;
+
+}
+
+
+void GameLoop() {
+	bool running = true;
+	std::unordered_set<std::string> pokedex = CreatePokedex();
+	while (running) {
+		std::string next_move = "none";
+		std::cout << "Please type your next move" << std::endl;
+		std::cin >> next_move;
+		if (next_move == "none") {
+			std::cout << "No move selected, Game will terminate" << std::endl;
+			return;
+		}
+		std::cout << "Your next move is: " << next_move << std::endl;
+	}
+}
+
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	std::cout << "Hello CMake." << std::endl;
+	std::cout << "Starting from day 1" << std::endl;
+	GameLoop();
 	return 0;
 }
